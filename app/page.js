5bf9ea1a7a66968be66a478d98075db2896213da
kjh9410065-art <kjh9410065@gmail.com>
2026-09-08 11:31:46 +0900

    Load recommendation card polish layer

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ import { rankServices } from "./lib/recommendation";
 import { matchesSearch } from "./lib/search";
 import "./home.css";
 import "./home-responsive.css";
+import "./home-polish.css";
 
 // 목적 카드에는 운영체제별 문자가 아니라 HUB 전용 SVG 일러스트만 사용합니다.
 // 각 목적은 추천 페이지의 같은 task 파라미터로 연결되어 홈과 추천 흐름이 이어집니다.

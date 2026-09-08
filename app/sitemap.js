e7bdf6fdbcb731f0d1f15fee09fdf7dd5f7f6766
kjh9410065-art <kjh9410065@gmail.com>
2026-09-08 19:39:02 +0900

    Fix static sitemap export build

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,5 +1,8 @@
 import { catalog } from "./lib/catalog";
 
+// static export 환경에서는 sitemap도 정적 라우트로 명시해야 빌드가 안정적으로 완료됩니다.
+export const dynamic = "force-static";
+
 // 공개 페이지와 서비스 상세 페이지를 검색엔진에 명확하게 전달합니다.
 export default function sitemap() {
   const baseUrl = "https://hub.carpick.workers.dev";

d03810af3ba8a18ae6576963692a90ec55489231
kjh9410065-art <kjh9410065@gmail.com>
2026-09-09 07:46:02 +0900

    Load mobile floating control fixes

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./responsive.css";
 import "./home-typography.css";
 import "./readable-ui.css";
 import "./components/adsense.css";
+import "./components/hub-floating-controls.css";
 import Script from "next/script";
 import HubTutorial from "./components/hub-tutorial";
 import HubTheme from "./components/hub-theme";

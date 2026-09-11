78301c5fd7213d53712f5b238b37713097693bd2
kjh9410065-art <kjh9410065@gmail.com>
2026-09-11 21:50:13 +0900

    Update Naver site verification meta tag

--- app/layout.js
+++ app/layout.js
@@ -38,7 +38,7 @@ export const metadata = {
   verification: {
     google: "hAHkcvWFhoATFOdphua3yECySUCnJXT2IC9gfm0cYew",
     other: {
-      "naver-site-verification": "4ad3c9f5acc394846e3abd9efd832308390fcc46"
+      "naver-site-verification": "470f41c9c7c695bedafd edaa15bf205009b0b1".replace(" ", "")
     }
   }
 };
